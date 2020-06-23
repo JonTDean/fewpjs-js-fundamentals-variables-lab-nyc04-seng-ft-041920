fb29e04f47bee523928025cf5af315c8fb6f53d8
amer519 <afahmy519@gmail.com>
2020-06-23 15:39:42 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputButton = document.querySelector('input[type="submit"]');
+    const ul = document.getElementById('tasks');
+
+
+  inputButton.addEventListener('click', function(x){
+    event.preventDefault()
+    newlist()
+    
+
+
+  })
+
+
+ function newlist() {
+     let newli = document.createElement('li');
+       ul.appendChild(newli);
+
+
+ }
+
+
+
+
 });
